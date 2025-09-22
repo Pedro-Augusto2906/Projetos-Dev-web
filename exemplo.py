44863da65e9618cb3f02c30bc5c0d3449cffad5e
unknown <augustopedro051@gmal.com>
2025-09-22 10:07:34 -0300

    Atualização

--- exemplo.py
+++ exemplo.py
@@ -1,2 +1,10 @@
-print('ola, mundo, primeiro teste')
-print ('teste 2')
+class ShoppingListManager:
+    def __init__(self):
+        self.shopping_list = [] 
+        def add_item(self, item):
+            # adciona um item á lista de compras
+           self.shopping_list.append(item)
+           print
+            # remove um item da lista de compras
+           def remove_item(self, item):
+            # Exibi a lista de compras
